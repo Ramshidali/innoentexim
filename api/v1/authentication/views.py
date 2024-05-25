6b9173d6cc6dd3e4a4e08345e8e1bc3e9b7bc528
Ramshidali <aliramshid@gmail.com>
2024-05-25 23:23:30 +0530

    update

--- api/v1/authentication/views.py
+++ api/v1/authentication/views.py
@@ -15,7 +15,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework import status
 
 from main.functions import decrypt_message, encrypt_message, get_otp, send_email
-from api.v1.authentication.serializers import InvestorSerializer, ResetPasswordSerializer, UserSerializer, LogInSerializer, UserTokenObtainPairSerializer
+from api.v1.authentication.serializers import ExecutiveSerializer, InvestorSerializer, ResetPasswordSerializer, UserSerializer, LogInSerializer, UserTokenObtainPairSerializer
 from api.v1.authentication.functions import generate_serializer_errors, get_user_token
 
 
@@ -95,15 +95,18 @@ def side_profile(request):
     user = request.user
     
     if not user.is_superuser:
-        if user.groups(name__in=["executive"]):
+        if user.groups.filter(name="executive").exists():
             instance = Executive.objects.get(user=user)
-            serializer = InvestorSerializer(instance,many=False)
-        elif user.groups(name__in=["investor"]):
+            serializer = ExecutiveSerializer(instance)
+        elif user.groups.filter(name="investor").exists():
             instance = Investors.objects.get(user=user)
-            serializer = InvestorSerializer(instance,many=False)
+            serializer = InvestorSerializer(instance)
+        else:
+            # Handle the case where the user is neither an executive nor an investor
+            raise ValueError("User does not belong to a recognized group")
     else:
-        instance = User.objects.get(pk=request.user.id)
-        serializer = UserSerializer(instance,many=False)
+        instance = User.objects.get(pk=user.id)
+        serializer = UserSerializer(instance)
         
     
     response_data = {
