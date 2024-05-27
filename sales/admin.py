4774078c1cdc2949642d65633ee9e18d006c75ff
Ramshidali <aliramshid@gmail.com>
2024-05-27 21:57:12 +0530

    update

--- sales/admin.py
+++ sales/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from sales.models import Sales
+from sales.models import *
 
 # Register your models here.
 admin.site.register(Sales)
+admin.site.register(SalesItems)
+admin.site.register(SalesExpenses)
+admin.site.register(SalesStock)
