dad6f0cd4603c06e0f396409ff4b16957b684bbe
Ramshidali <aliramshid@gmail.com>
2024-06-20 23:15:28 +0530

    update

--- main/views.py
+++ main/views.py
@@ -34,22 +34,34 @@ def app(request):
 def index(request):
     purchase = Purchase.objects.filter(date=datetime.today().date(),is_deleted=False)
     todays_purchases = PurchasedItems.objects.filter(purchase__in=purchase,is_deleted=False).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
+    purchase_expense = PurchaseExpense.objects.filter(purchase__in=purchase,is_deleted=False).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
     sales = Sales.objects.filter(date=datetime.today().date(),is_deleted=False)
     todays_sales = SalesItems.objects.filter(sales__in=sales,is_deleted=False).aggregate(total_amount=Sum('amount_in_inr'))['total_amount'] or 0
-    
-    purchase_expense = PurchaseExpense.objects.filter(purchase__in=purchase,is_deleted=False).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
     sales_expense = SalesExpenses.objects.filter(sales__in=sales,is_deleted=False).aggregate(total_amount=Sum('amount_in_inr'))['total_amount'] or 0
     other_expences = OtherExpences.objects.filter(date_added__date=datetime.today().date(),is_deleted=False).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
     
     expenses = purchase_expense + sales_expense + other_expences
-    
     profit = todays_sales - todays_purchases - purchase_expense - sales_expense - other_expences
+    
+    monthly_purchase = PurchasedItems.objects.filter(purchase__date__month=datetime.today().date().month,is_deleted=False).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
+    monthly_purchase_expense = PurchaseExpense.objects.filter(purchase__date__month=datetime.today().date().month,is_deleted=False).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
+    monthly_sales = SalesItems.objects.filter(sales__date__month=datetime.today().date().month,is_deleted=False).aggregate(total_amount=Sum('amount_in_inr'))['total_amount'] or 0
+    monthly_sales_expense = SalesExpenses.objects.filter(sales__date__month=datetime.today().date().month,is_deleted=False).aggregate(total_amount=Sum('amount_in_inr'))['total_amount'] or 0
+    monthly_other_expences = OtherExpences.objects.filter(date_added__month=datetime.today().date().month,is_deleted=False).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
+    
+    monthly_expenses = monthly_purchase + monthly_sales_expense + monthly_other_expences
+    monthly_profit = monthly_sales - monthly_purchase - monthly_purchase_expense - monthly_sales_expense - monthly_other_expences
     
     context = {
         'todays_purchases': todays_purchases,
         'todays_sales': todays_sales,
         'expenses': expenses,
         'profit' : profit,
+        
+        'monthly_purchase': monthly_purchase,
+        'monthly_sales': monthly_sales,
+        'monthly_expenses': monthly_expenses,
+        'monthly_profit': monthly_profit,
         
         'page_name' : 'Dashboard',
         'page_title' : 'Dashboard | Innoentexim',
